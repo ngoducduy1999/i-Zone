f770d4980b21068876e8fc8d8a56097e04e7e66a
anhnvph37958 <anhnvph37958@fpt.edu.vn>
2024-09-26 20:38:57 +0700

    sửa migration mau sac

--- database/migrations/2024_09_12_052342_create_mau_sacs_table.php
+++ database/migrations/2024_09_12_052342_create_mau_sacs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mau_sacs', function (Blueprint $table) {
             $table->id();
             $table->string('ten_mau_sac');
+            $table->boolean('trang_thai')->default(true);
             $table->timestamps();
             $table->softDeletes();
         });
