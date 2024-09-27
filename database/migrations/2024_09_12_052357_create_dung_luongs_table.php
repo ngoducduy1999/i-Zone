81f552744ff16084a8a1a12018290aaff19ae201
anhnvph37958 <anhnvph37958@fpt.edu.vn>
2024-09-27 20:00:00 +0700

    them lai trang thai trong migration

--- database/migrations/2024_09_12_052357_create_dung_luongs_table.php
+++ database/migrations/2024_09_12_052357_create_dung_luongs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('dung_luongs', function (Blueprint $table) {
             $table->id();
             $table->string('ten_dung_luong');
+            $table->boolean('trang_thai')->default(true);
             $table->timestamps();
             $table->softDeletes();
         });
