a262d80102a18668058e31b9a47abd1c9436d551
tuanbon-source <tuandmph44484@fpt.edu.vn>
2024-10-04 21:29:53 +0700

    Giải quyết xung đột

--- resources/views/admins/sanphams/index.blade.php
+++ resources/views/admins/sanphams/index.blade.php
@@ -44,7 +44,10 @@
                                             <ul>
                                                 <li>Mã sản phẩm: {{ $sanpham->ma_san_pham }}</li>
                                                 <li>Tên sản phẩm: {{ $sanpham->ten_san_pham }}</li>
+                                                @if($sanpham->danhMuc && $sanpham->danhMuc->ten_danh_muc != "")
                                                 <li>Danh mục: {{ $sanpham->danhMuc->ten_danh_muc }}</li>
+                                                @endif
+
                                                 <li>Ảnh : <br> <img src="{{ asset($sanpham->anh_san_pham) }}"
                                                         alt="{{ $sanpham->ten_san_pham }}" width="50px"></li>
                                                 <li>Ngày tạo: {{ $sanpham->created_at->format('d-m-Y') }}</li>
