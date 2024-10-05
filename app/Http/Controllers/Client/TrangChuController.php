3c51609938a4eaf1e70f133fb96a9bc81b39b80d
tuanbon-source <tuandmph44484@fpt.edu.vn>
2024-10-05 11:41:30 +0700

    khuyến mãi

--- app/Http/Controllers/Client/TrangChuController.php
+++ app/Http/Controllers/Client/TrangChuController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\SanPham;
 use App\Models\Banner;
+use App\Models\KhuyenMai;
 
 class TrangChuController extends Controller
 {
@@ -17,6 +18,14 @@ class TrangChuController extends Controller
         $banners1 = Banner::where('trang_thai', 1)->orderBy('created_at', 'desc')->skip(1)->first(); // Bản ghi mới thứ 2
         $banners2 = Banner::where('trang_thai', 1)->orderBy('created_at', 'desc')->skip(2)->first(); // Bản ghi mới thứ 3
          // Trả về view và truyền dữ liệu banners sang
-         return view('clients.trangchu', compact('banners', 'banners1', 'banners2'));
+
+         // Lấy danh sách khuyến mãi còn hiệu lực
+         $khuyenMais = KhuyenMai::where('trang_thai', 1) // Kiểm tra trạng thái
+         ->where('ngay_ket_thuc', '>=', now()) // Kiểm tra ngày kết thúc
+         ->orderBy('ngay_ket_thuc', 'asc') // Sắp xếp theo ngày kết thúc gần nhất
+         ->take(3) // Lấy 3 khuyến mãi
+         ->get();
+
+       return view('clients.trangchu', compact('khuyenMais','banners', 'banners1', 'banners2'));
     }
 }
