a52f02d537d3b62a3cad71c4e5a9ec3f4492c110
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-11-08 21:31:02 +0700

    dongbo them tieng viet cho trang chi tiết

--- resources/views/clients/chitietsanpham.blade.php
+++ resources/views/clients/chitietsanpham.blade.php
@@ -413,7 +413,7 @@
                                         </svg>
                                     @endif
                                     </span>
-                                    Add Wishlist
+                                    Thêm vào sản phẩm yêu thích
                                 </button>
                             @else
                                 {{-- <button type="button" class="tp-product-details-action-sm-btn">
