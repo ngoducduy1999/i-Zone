dcaf5f8c8265eda26edc979d3e16834e6d828db1
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-11-08 20:04:15 +0700

    thêm html vào trang dữ liệu

---
+++ resources/views/clients/gioithieu.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.client')
+
+@section('content')
+<div class="d-flex">
+
+
+    <!-- Content -->
+    <div class="content p-4 flex-grow-1 ">
+        <h1 class="text-center">GIỚI THIỆU VỀ TOPZONE - CHUỖI CỬA HÀNG CÔNG NGHỆ CAO CẤP</h1>
+        <p>TopZone là kết quả của sự hợp tác chặt chẽ giữa Apple và Thế Giới Di Động. TopZone mang đến cho cộng đồng iFan đông đảo không gian trải nghiệm mua sắm đẳng cấp với phiên bản cửa hàng ủy quyền chính thức được nâng cấp và cập nhật mới nhất được đưa về Việt Nam.</p>
+        <img src="{{asset('')}}" alt="">
+    </div>
+</div>
+
+@endsection
