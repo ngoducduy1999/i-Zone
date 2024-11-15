68efe4785b34074029e01bcc5a69a72732e47bf8
ngoducduy1999 <dngo2910@gmail.com>
2024-11-15 22:54:09 +0700

    fix trang chủ

--- resources/views/clients/trangchu.blade.php
+++ resources/views/clients/trangchu.blade.php
@@ -78,7 +78,7 @@
 
 <!-- product category area end -->
 
-<!-- feature area start -->
+{{-- <!-- feature area start -->
 <section class="tp-feature-area tp-feature-border-radius pb-70">
    <div class="container">
       <div class="row gx-1 gy-1 gy-xl-0">
@@ -157,7 +157,7 @@
          </div>
       </div>
    </div>
-</section>
+</section> --}}
 
 <!-- product area start -->
 <section class="tp-product-area pb-55">
@@ -530,101 +530,7 @@
    </div>
 </section>
 
-<!-- product banner area start -->
-<div class="tp-product-banner-area pb-90">
-   <div class="container">
-      <div class="tp-product-banner-slider fix">
-         <div class="tp-product-banner-slider-active swiper-container">
-            <div class="swiper-wrapper">
-               <div class="tp-product-banner-inner theme-bg p-relative z-index-1 fix swiper-slide">
-                  <h4 class="tp-product-banner-bg-text">Máy tính bảng</h4>
-                  <div class="row align-items-center">
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-content p-relative z-index-1">
-                           <span class="tp-product-banner-subtitle">Bộ sưu tập máy tính bảng 2023</span>
-                           <h3 class="tp-product-banner-title">Samsung Galaxy Tab S6, Wifi Tablet</h3>
-                           <div class="tp-product-banner-price mb-40">
-                              <span class="old-price">$1240.00</span>
-                              <p class="new-price">$975.00</p>
-                           </div>
-                           <div class="tp-product-banner-btn">
-                              <a href="shop.html" class="tp-btn tp-btn-2">Mua ngay</a>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-thumb-wrapper p-relative">
-                           <div class="tp-product-banner-thumb-shape">
-                              <span class="tp-product-banner-thumb-gradient"></span>
-                              <img class="tp-offer-shape" src="{{ asset('assets/client/img/banner/banner-slider-offer.png') }}" alt="">
-                           </div>
-                           <div class="tp-product-banner-thumb text-end p-relative z-index-1">
-                              <img src="{{ asset('assets/client/img/banner/banner-slider-1.png') }}" alt="">
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="tp-product-banner-inner theme-bg p-relative z-index-1 fix swiper-slide">
-                  <h4 class="tp-product-banner-bg-text">tablet</h4>
-                  <div class="row align-items-center">
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-content p-relative z-index-1">
-                           <span class="tp-product-banner-subtitle">Latest Technology Added</span>
-                           <h3 class="tp-product-banner-title">Apple iPad 10.2 9th Gen - 2021</h3>
-                           <div class="tp-product-banner-price mb-40">
-                              <span class="old-price">$1450.00</span>
-                              <p class="new-price">$1199.00</p>
-                           </div>
-                           <div class="tp-product-banner-btn">
-                              <a href="shop.html" class="tp-btn tp-btn-2">Shop now</a>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-thumb-wrapper p-relative">
-                           <div class="tp-product-banner-thumb-shape">
-                              <span class="tp-product-banner-thumb-gradient"></span>
-                              <img class="tp-offer-shape" src="{{ asset('assets/client/img/banner/banner-slider-offer.png') }}" alt="">
-                           </div>
-                           <div class="tp-product-banner-thumb text-end p-relative z-index-1">
-                              <img src="{{ asset('assets/client/img/banner/banner-slider-2.png') }}" alt="">
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="tp-product-banner-inner theme-bg p-relative z-index-1 fix swiper-slide">
-                  <h4 class="tp-product-banner-bg-text">tablet</h4>
-                  <div class="row align-items-center">
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-content p-relative z-index-1">
-                           <span class="tp-product-banner-subtitle">Tablet Collection 2023</span>
-                           <h3 class="tp-product-banner-title">Microsoft Surface Pro 8, Wifi Included</h3>
-                           <div class="tp-product-banner-price mb-40">
-                              <span class="old-price">$1249.00</span>
-                              <p class="new-price">$1300.00</p>
-                           </div>
-                           <div class="tp-product-banner-btn">
-                              <a href="shop.html" class="tp-btn tp-btn-2">Shop now</a>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="col-xl-6 col-lg-6">
-                        <div class="tp-product-banner-thumb-wrapper p-relative">
-                           <div class="tp-product-banner-thumb-shape">
-                              <span class="tp-product-banner-thumb-gradient"></span>
-                              <img class="tp-offer-shape" src="{{ asset('assets/client/img/banner/banner-slider-offer.png') }}" alt="">
-                           </div>
-                           <div class="tp-product-banner-thumb text-end p-relative z-index-1">
-                              <img src="{{ asset('assets/client/img/banner/banner-slider-3.png') }}" alt="">
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="tp-product-banner-slider-dot tp-swiper-dot"></div>
+
 
        <!-- banner area start -->
        <section class="tp-banner-area pb-70">
