fff30d2885988be4377d8b81555f57a7d742ed2c
ngoducduy1999 <dngo2910@gmail.com>
2024-11-16 21:15:21 +0700

    mghn

---
+++ config/ghn.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_url' => env('GHN_API_URL', 'https://dev-online-gateway.ghn.vn/shiip/public-api/v2'),
+    'api_token' => env('GHN_API_TOKEN'),
+    'shop_id' => env('GHN_SHOP_ID'),
+];
