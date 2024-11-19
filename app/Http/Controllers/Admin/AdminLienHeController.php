47038a3bc699556bf7ad21f4c593350c64cc9a58
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-11-19 22:10:18 +0700

    adminlienhe

--- app/Http/Controllers/Admin/AdminLienHeController.php
+++ app/Http/Controllers/Admin/AdminLienHeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\lien_hes;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Mail\PhanHoiMail;
 use App\Models\User;
+use App\Models\lien_hes;
+use App\Mail\PhanHoiMail;
+use Illuminate\Http\Request;
+use App\Mail\CustomerReplyMail;
+use App\Http\Controllers\Controller;
+use App\Models\Adminphanhoi;
 use Illuminate\Support\Facades\Mail;
 
 class AdminLienHeController extends Controller
@@ -51,11 +53,36 @@ class AdminLienHeController extends Controller
         return redirect()->back()->with('success', 'Tin nhắn đã được gửi!');
     }
 
+    public function sendReply(Request $request, $id)
+    {
+        $request->validate([
+            'reply' => 'required|string',
+        ]);
+    
+        // Lưu phản hồi của quản trị viên vào database
+        $reply = Adminphanhoi::create([
+            'lien_hes_id' => $id,
+            'reply' => $request->reply,
+        ]);
+    
+        // Lấy email của khách hàng
+        $lienhes = lien_hes::findOrFail($id);
+    
+        // Gửi email
+        Mail::to($lienhes->user->email)->send(new CustomerReplyMail($reply->reply));
+    
+        return redirect()->route('admin.lienhes.index')->with('success', 'Phản hồi đã được gửi thành công!');
+    }
 
 
-
-
+    public function showReplyForm($id)
+    {
+        $lienhes = lien_hes::findOrFail($id); // Tìm phản hồi theo ID
+    
+        return view('admins.lienhes.phanhoi', compact('lienhes'));
+    }
    
+    
     /**
      * Display the specified resource.
      */
