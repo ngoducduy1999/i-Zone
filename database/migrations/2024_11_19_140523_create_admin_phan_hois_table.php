a2072922ff1d3382bf0b7978dbbeeb71b98829cb
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-11-19 22:17:11 +0700

    tạo bảng phản hòi

---
+++ database/migrations/2024_11_19_140523_create_admin_phan_hois_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_phan_hois', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lien_hes_id')->constrained('lien_hes')->onDelete('cascade');
+            $table->text('reply');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_phan_hois');
+    }
+};
