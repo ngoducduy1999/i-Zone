e1e13ac761d1d05f52e1b646165c53e2c7b1685d
ngoducduy1999 <dngo2910@gmail.com>
2024-11-21 20:05:50 +0700

    up date và fix loi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,10 +10,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+    protected function schedule(Schedule $schedule)
+{
+    $schedule->command('orders:delete-expired')->everyMinute(); // Chạy mỗi phút
+}
+
 
     /**
      * Register the commands for the application.
