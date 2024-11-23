bccef2c4ebff0e4582004f8ae9a43c0d4368fdf9
anhnt-theanh26 <nguyentheanh260204@gmail.com>
2024-11-23 07:30:45 +0700

    anhnt đổi danh mục thành dropdow trang client và header

--- resources/views/clients/block/header.blade.php
+++ resources/views/clients/block/header.blade.php
@@ -351,13 +351,14 @@ $(document).ready(function() {
                             <li>
                                <a href="{{ route('san-pham') }}">Sản phẩm</a>
                             </li>
-                            @foreach($danhMucs as $danhMuc)
-                              <li>
-                                 <a href="{{ route('sanpham.danhmuc', ['danh_muc_id' => $danhMuc->id]) }}">
-                                    {{ $danhMuc->ten_danh_muc }}
-                                 </a>
-                              </li>
-                           @endforeach
+                            <li class="has-dropdown">
+                              <a href="{{ route('san-pham') }}">Danh mục</a>
+                              <ul class="tp-submenu">
+                                  @foreach ($danhMucs as $danhMuc)
+                                      <li><a href="{{ route('san-pham') }}?danh_muc={{ $danhMuc->id }}">{{$danhMuc->ten_danh_muc}}</a></li>
+                                  @endforeach
+                              </ul>
+                           </li>
                             <li>
                                <a href="{{ route('bai-viet') }}">Tin tức</a>
                             </li>
