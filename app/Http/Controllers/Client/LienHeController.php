db7e338b2b32db98e7a0bd41a2750652ecbbd627
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-11-28 15:15:21 +0700

    thêm validate

--- app/Http/Controllers/Client/LienHeController.php
+++ app/Http/Controllers/Client/LienHeController.php
@@ -23,6 +23,23 @@ class LienHeController extends Controller
             return redirect()->back()->with('error', 'Bạn cần đăng nhập để gửi form liên hệ');
         }
 
+
+        $validatedData = $request->validate(
+            [
+                'ten_nguoi_gui' => 'required|string|max:255',
+                'tin_nhan' => 'nullable|string',
+                
+            ],
+            [
+                'ten_nguoi_gui.required' => 'Tên người gửi được để trống',
+                'ten_nguoi_gui.string' => 'Tên người gửi phải là chuỗi',
+               
+                'ten_nguoi_gui.max' => 'Tên người gửi  không quá 255 ký tự',
+
+                'tin_nhan.nullable' => 'Tin nhắn không được bỏ trống khi gửi',
+                'tin_nhan.string' => 'Tin nhắn phải là chuỗi',
+            ]
+        );
         // Lấy dữ liệu từ form
         $ten_nguoi_gui = $request->input('ten_nguoi_gui');
         $tin_nhan = $request->input('tin_nhan');
