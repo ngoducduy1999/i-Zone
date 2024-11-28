6711df4ae6192496c2767c19d015193503d5bc8d
ngoducduy1999 <dngo2910@gmail.com>
2024-11-28 12:00:57 +0700

    cap nhat view gui mail

--- resources/views/emails/invoice_created.blade.php
+++ resources/views/emails/invoice_created.blade.php
@@ -105,7 +105,7 @@
         </table>
         <div class="footer">
             <p>Cảm ơn bạn đã mua sắm tại cửa hàng của chúng tôi!</p>
-            <p>Mọi thắc mắc xin vui lòng liên hệ <a href="mailto:support@example.com">support@example.com</a>.</p>
+            <p>Mọi thắc mắc xin vui lòng liên hệ <a href="mailto:loviongs@gmai.com">loviongs@gmai.com</a>.</p>
         </div>
     </div>
 </body>
