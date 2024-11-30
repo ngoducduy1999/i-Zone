3690ec6833c2b9ea2b02a34a6a5ba13c51973151
Xuanviet1110 <vietnxph41571@fpt.edu.vn>
2024-11-30 10:38:09 -0500

    tạo bảng trả lời

--- database/migrations/2024_11_28_211859_create_tra_lois_table.php
+++ database/migrations/2024_11_28_211859_create_tra_lois_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('tra_lois', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('danh_gia_id'); // ID của đánh giá
+            $table->unsignedBigInteger('user_id'); // ID của người dùng
+            $table->text('noi_dung'); // Nội dung trả lời
             $table->timestamps();
+
+            // Khóa ngoại
+            $table->foreign('danh_gia_id')->references('id')->on('danh_gia_san_phams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
