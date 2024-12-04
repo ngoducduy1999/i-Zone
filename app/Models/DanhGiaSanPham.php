b6224290ca32ceecae2ff34919e00dc2dd66c45f
Xuanviet1110 <vietnxph41571@fpt.edu.vn>
2024-12-04 08:20:34 -0500

    vietnx update model danhgiasanpham

--- app/Models/DanhGiaSanPham.php
+++ app/Models/DanhGiaSanPham.php
@@ -28,4 +28,23 @@ class DanhGiaSanPham extends Model
     {
         return $this->hasMany(TraLoi::class, 'danh_gia_id');
     }
+    public function bienTheSanPhams()
+    {
+        return $this->hasMany(BienTheSanPham::class, 'san_pham_id', 'san_pham_id');
+    }
+    public function chiTietHoaDons()
+    {
+        return $this->hasMany(ChiTietHoaDon::class, 'san_pham_id', 'san_pham_id');
+    }
+    // In the DanhGiaSanPham model
+public function replies()
+{
+    return $this->hasMany(TraLoi::class, 'danh_gia_id');
+}
+// Define the relationship with BienTheSanPham
+public function bienTheSanPham()
+{
+    return $this->belongsTo(BienTheSanPham::class, 'bien_the_san_pham_id');
+}
+
 }
