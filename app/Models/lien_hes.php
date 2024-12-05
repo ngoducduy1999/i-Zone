e47bd1648b23228766a378796bd90e220107524f
LeVietAnh27 <anhlvph42212@fpt.edu.vn>
2024-12-05 13:51:05 +0700

    b

--- app/Models/lien_hes.php
+++ app/Models/lien_hes.php
@@ -2,22 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class lien_hes extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     protected $table = 'lien_hes';
 
     // Các cột được phép gán giá trị hàng loạt
+
+    const STATUS_PENDING = 'pending';
+    const STATUS_RESOLVED = 'resolved';
+    const STATUS_DELETED = 'deleted';
+
+    
     protected $fillable = [
         'ten_nguoi_gui',
-        'email_nguoi_gui',
+     
         'tin_nhan',
-        'trang_thai_tra_loi',
+        'trang_thai_phan_hoi',
         'user_id'
+
     ];
 
     public function user()
