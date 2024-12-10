66e65fa2fd68ee89d21b423037461cfc65c0bb6c
ngoducduy1999 <dngo2910@gmail.com>
2024-12-11 02:40:22 +0700

    api ok

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Client\DanhgiaController;
-use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
